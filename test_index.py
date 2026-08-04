f3b243913af80b21d49bacf116d7fb5970499210
Manh Nguyen <nguyenmanh1904ht@gmail.com>
2026-08-04 16:42:27 +0700

    Delete test_index.py

--- test_index.py
+++
@@ -1,6 +0,0 @@
-from pageindex.client import PageIndexClient
-import inspect
-
-print(inspect.signature(PageIndexClient.submit_document))
-print(inspect.signature(PageIndexClient.submit_query))
-print(inspect.signature(PageIndexClient.get_retrieval))
